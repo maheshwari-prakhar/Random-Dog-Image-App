b6cfe5de87697d3b698e158c46bfb212ce297ece
maheshwari-prakhar <prakhar06.maheshwari@gmail.com>
2020-01-01 20:16:42 -0800

    Initial Project Setup

---
+++ Random Dog Image/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Random Dog Image
+//
+//  Created by Prakhar Maheshwari on 1/1/20.
+//  Copyright © 2020 Prakhar Maheshwari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
